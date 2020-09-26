eed6f46b9afec43fbc3bb1caa331226d39707668
Jordan Turley <ztizzlegaming@gmail.com>
2020-09-26 00:16:38 -0400

    Bug fix for alphabet

--- pw.c
+++ pw.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -15,13 +16,14 @@ int main(int argc, char *argv[]) {
 	srand(time(0));
 
 	//Init characters we can draw from
-	const *S = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char *S = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const int A_LEN = strlen(S);
 
 	//Loop n times and pick a random character
 	int r;
 	char c;
 	for (int i1 = 0; i1 < n; i1++) {
-		r = rand() % 72;
+		r = rand() % A_LEN;
 		c = S[r];
 		printf("%c", c);
 	}
